b54f4980f22132d6996d41412f812f6a057ce691
ggospodinov <baigrozdan99@abv.bg>
2022-12-26 22:12:58 +0200

    first commit

---
+++ src/js/app.js
@@ -0,0 +1,7 @@
+import "../scss/app.scss";
+
+window.addEventListener("DOMContentLoaded", () => {
+  // This block will be executed once the page is loaded and ready
+
+  const ul = document.querySelector("ul");
+});
