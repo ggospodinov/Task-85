3885df3bda013f64968ea08f8f06c88b76e1069f
ggospodinov <baigrozdan99@abv.bg>
2022-12-26 22:29:55 +0200

    add change

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,19 @@ import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
-
+ const pokemonCount = 10;
   const ul = document.querySelector("ul");
+
+  fetch(`https://pokeapi.co/api/v2/pokemon/?limit=10`)
+  .then((response)=> response.json())
+  .then((data)=>{
+    // console.log(data);
+    data.results.forEach((pokemon) => {
+      // console.log(pokemon)
+
+      let li = document.createElement('li');
+        li.innerText = pokemon.name
+        ul.appendChild(li)
+    });
+  })
 });
